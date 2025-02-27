2d73d160755408d3541fb93cc31510b0014141d3
Kenneth Chua <cloudberry2011a@hotmail.com>
2025-02-27 15:45:06 +0800

    Create the Entry File

---
+++ data_entry_app.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+root.title("ABQ Data Entry Application")
+root.columnconfigure(0, weight=1)
+
+# Start coding here
+
+root.mainloop()
