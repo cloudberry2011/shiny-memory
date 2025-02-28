f9394d9c7fa6af6b3061d2bb8ab4ff89da3461fd
Kenneth Chua <cloudberry2011a@hotmail.com>
2025-02-28 16:35:12 +0800

    Create the application heading

--- data_entry_app.py
+++ data_entry_app.py
@@ -7,4 +7,11 @@ root.columnconfigure(0, weight=1)
 
 # Start coding here
 
+# Application heading
+ttk.Label(
+    root,
+    text="ABQ Data Entry Application",
+    font=("TkDefaultFont", 16)
+).grid()
+
 root.mainloop()
