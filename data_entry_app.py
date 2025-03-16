ed810fc8da636a530f78ad76dd1c9bc6771765ba
Kenneth Chua <cloudberry2011a@hotmail.com>
2025-03-17 04:30:29 +0800

    Create the Blossoms Field
    - create the variable
    - create the label
    - create the field

--- data_entry_app.py
+++ data_entry_app.py
@@ -143,6 +143,17 @@ ttk.Spinbox(
     to=20,
     increment=1
 ).grid(row=1, column=0, sticky=(tk.W + tk.E))
+
+# Blossoms
+variables['Blossoms'] = tk.IntVar()
+ttk.Label(p_info, text='Blossoms').grid(row=0, column=1)
+ttk.Spinbox(
+    p_info,
+    textvariable=variables['Blossoms'],
+    from_=0,
+    to=1000,
+    increment=1
+).grid(row=1, column=1, sticky=(tk.W + tk.E))
         
 root.mainloop()
 
